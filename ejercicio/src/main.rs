312d209975f4af2136ff7a7854a767ec597d800e
Londro24 <alejandro.ide.figueroa@gmail.com>
2022-11-09 21:26:23 -0300

    Completado.

--- ejercicio/src/main.rs
+++ ejercicio/src/main.rs
@@ -1,73 +1,88 @@
+use std::io::stdin;
 use std::fs::File;
 use std::path::Path;
 use std::io::Read;
 use std::fs::OpenOptions;
 use std::io::Write;
 
-
 #[derive(Default)]
-struct Persona{ //creación de estrcutura
+struct Persona{ 
     nombre: String,
     edad: u8,
     rut: String,
     mascota: [Mascota; 5]
 }
-
+// Creación de estrcutura
 
 #[derive(Default)]
-struct Mascota{ //creación de sub-estrcutura
+struct Mascota{
     nombre: String, 
     tipo: String,
     color: String,
 }
+// Creación de sub-estrcutura
 
-
-fn read_file(mut f: &File){ //Muestra el texto del archivo
+fn read_file(mut file: &File){
     let mut text = String::new();
-    f.read_to_string(&mut text).unwrap();
+    file.read_to_string(&mut text).unwrap();
     println!("{}", &text);
 }
+// Muestra el texto del archivo
 
-
-fn create_blank_file(p: &Path){ //Crea el archivo si no existe
-    let _file = File::create(p).expect("El archivo no pudo crearse");
-    println!("El archivo fue creado");
+fn have_file(mut file: &File) -> String{
+    let mut text = String::new();
+    file.read_to_string(&mut text).unwrap();
+    return text
 }
+// Toma el texto del archivo
 
+fn create_blank_file(path: &Path){ 
+    let _file = File::create(path).expect("El archivo no pudo crearse");
+}
+// Crea el archivo si no existe
 
-/* //Añade texto
-fn add_new_content(mut f: &File){ 
-    f.write_all(b"Nuevo texto\n");
-} 
-*/
-
-
-fn open_file_to_append(p: &Path) -> File{ //Abre el archivo para escribir en el
+fn open_file_to_append(path: &Path) -> File{
     let mut binding = OpenOptions::new();
     let binding = binding.append(true);
-    let file = match binding.open(p){
+    let file = match binding.open(path){
         Err(_why) => panic!("No se puede abrir el archivo"),
         Ok(file) => file,
     };
     //
     return file
 }
+// Abre el archivo para escribir en el
 
-
-fn open_file_to_read(p: &Path){ //Verifica si existe
-    if Path::new(p).exists(){
-        let file = match File::open(&p){
+fn open_file_to_read(path: &Path){ 
+    if Path::new(path).exists(){
+        let file = match File::open(&path){
             Err(_why) => panic!("El archivo no se puede abrir..."),
             Ok(file) => file,
         };
         read_file(&file)
     } else {
-        create_blank_file(p);
+        create_blank_file(path);
+        println!("Se creo el archivo");
     }
 }
+// Verifica si existe
 
+fn open_file_to_have(path: &Path) -> String{ 
+    if Path::new(path).exists(){
+        let file = match File::open(&path){
+            Err(_why) => panic!("El archivo no se puede abrir..."),
+            Ok(file) => file,
+        };
+        let text = have_file(&file);
+        return text;
+    } else {
+        create_blank_file(path);
+        return "Se creo el archivo".to_string();
+    }
+}
+// Verifica si existe2
 
-fn crear_personas() -> [Persona;3] { // Crea el array de personas
+fn create_personas() -> [Persona;3]{ 
     // Primera persona
     let m1 = Mascota{nombre:"Nora".to_string(),
         tipo:"Perro".to_string(),
@@ -120,21 +135,21 @@ fn crear_personas() -> [Persona;3] { // Crea el array de personas
     //
     return registro;
 }
+// Crea el array de personas
 
-
-fn registrar_persona(p: &Path, registro: [Persona;3]){ //Genera datos
+fn register_persona(path: &Path, registro: [Persona;3]){
     for persona in 0..3{
-        let mut temp = format!("{}:{}:{}", registro[persona].nombre, 
-        registro[persona].edad, 
-        registro[persona].rut
+        let mut temp = format!("{}:{}:{}",  registro[persona].nombre, 
+                                                    registro[persona].edad, 
+                                                    registro[persona].rut
         );
         //
         for mascota in 0..5 {
             if mascota != 4 && registro[persona].mascota[mascota].nombre != registro[persona].mascota[mascota + 1].nombre{
-                temp = format!("{}:{}:{}:{}", temp,
-                registro[persona].mascota[mascota].nombre, 
-                registro[persona].mascota[mascota].tipo, 
-                registro[persona].mascota[mascota].color, 
+                temp = format!("{}:{}:{}:{}",   temp,
+                                                registro[persona].mascota[mascota].nombre, 
+                                                registro[persona].mascota[mascota].tipo, 
+                                                registro[persona].mascota[mascota].color, 
                 );
             } else {
                 temp = format!("{}:\n", temp);
@@ -142,18 +157,140 @@ fn registrar_persona(p: &Path, registro: [Persona;3]){ //Genera datos
             }
         }
         //
-        let mut file = open_file_to_append(p);
+        let mut file = open_file_to_append(path);
         file.write_all(temp.as_bytes()).unwrap();
     }
 }
+// Genera datos
 
+fn add_texto(path: &Path) -> bool{
+    open_file_to_read(path);
+    let registro: [Persona;3] = create_personas();
+    register_persona(path, registro);
+    println!("Se han registrado datos");
+    return true
+}
+// Para agregar texto
+
+fn match_persona(line: &str) -> Persona{
+    let mut p_temp:Persona = Default::default();
+    let mut contador = 0;
+    //
+    for word in line.split(":"){
+        if contador == 0 {
+            p_temp.nombre = word.to_string();
+        } else if contador == 1 {
+            p_temp.edad = word.parse().unwrap();
+        } else if contador == 2 {
+            p_temp.rut = word.to_string();
+        } else if contador == 3 {
+            p_temp.mascota[0].nombre = word.to_string();
+        } else if contador == 4 {
+            p_temp.mascota[0].tipo = word.to_string();
+        } else if contador == 5 {
+            p_temp.mascota[0].color = word.to_string();
+        } else if contador == 6 {
+            p_temp.mascota[1].nombre = word.to_string();
+        } else if contador == 7 {
+            p_temp.mascota[1].tipo = word.to_string();
+        } else if contador == 8 {
+            p_temp.mascota[1].color = word.to_string();
+        }
+        contador += 1;
+    }
+    //
+    return p_temp;
+}
+//Asigna los datos del archivo a la array
+
+fn make_array(text: &str) -> [Persona;3]{
+    let mut p1:Persona = Default::default();
+    let mut p2:Persona = Default::default();
+    let mut p3:Persona = Default::default();
+    let mut contador = 0;
+    //
+    for line in text.split("\n"){
+        let p_temp:Persona = match_persona(line);
+        //
+        if contador == 0 {
+            p1 = p_temp;
+        } else if contador == 1{
+            p2 = p_temp;
+        } else if contador == 2 {
+            p3 = p_temp;
+        }
+        //
+        contador += 1;
+    }
+
+    return [p1,p2,p3];
+}
+//Hace array
+
+fn menu_2() -> String{
+    println!("Elija una persona\n[1] Sofía\n[2] Alejandro\n[3] Marcelo");
+    let mut numero = String::new();
+    stdin().read_line(&mut numero).unwrap();
+    return numero;
+}
+//menu para elegir las personas
+
+fn show_persona(num: &str, array:[Persona;3]) -> [Persona;3]{
+    if num == "1"{
+        println!("{} tiene un {} llamado {}", array[0].nombre, array[0].mascota[0].tipo, array[0].mascota[0].nombre)
+    } else if num == "2" {
+        println!("{} tiene un {} llamado {}", array[1].nombre, array[1].mascota[0].tipo, array[1].mascota[0].nombre)
+    } else if num == "3" {
+        println!("{} tiene un {} llamado {} y un {} llamado {}", array[2].nombre, array[2].mascota[0].tipo, array[2].mascota[0].nombre, array[2].mascota[1].tipo, array[2].mascota[1].nombre)
+    }
+    return array;
+}
+//Muestra en consola los datos de la array
+
+fn read_texto(path: &Path) -> bool {
+    let mut _romper = false;
+    let text = open_file_to_have(path);
+    //
+    loop{
+        let _array:[Persona;3] = make_array(&text);
+        println!("{}", _array[2].nombre);
+        let num = menu_2();
+        _romper = match num.trim() {
+            "1"|"2"|"3" => true,
+            _ => break
+        };
+        //
+        let _array = show_persona(&num.trim(), _array);
+        break;
+    }
+    //
+    return true
+}
+// Para leer texto
+
+fn menu() -> String{
+    println!("Elija un valor.\n[1] Agregar texto\n[2] Leer texto");
+    let mut numero = String::new();
+    stdin().read_line(&mut numero).unwrap();
+    return numero;
+}
+// Menu para decidir que hacer
 
 fn main(){
     let path = Path::new("ejemplo.txt");
-    open_file_to_read(path);
-    //let file = open_file_to_append(path);
-    //add_new_content(&file);
-    let registro: [Persona;3] = crear_personas();
-    registrar_persona(path, registro);
-    
+    let mut _romper = false;
+    println!("Bienvenido.");
+    //
+    loop {
+        let numero = menu();
+        _romper = match numero.trim() {
+            "1" => add_texto(path),
+            "2" => read_texto(path),
+            _ => false
+        };
+        //
+        if _romper {
+            break;
+        }
+    }
 }
